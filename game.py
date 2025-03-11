fb72470d120af33863f726feaa1364c79dbebf8d
Kea <keammakola@gmail.com>
2025-03-11 13:52:39 +0200

    Created deck

---
+++ game.py
@@ -0,0 +1,17 @@
+
+def create_deck():
+    deck = []
+    suits = ("hearts","diamonds","spades","clubs")
+    faces = ("jack","queen","king","ace")
+
+    for suit in suits:
+        for i in range(2,11):
+            deck.append((i,suit))
+        for face in faces:
+            deck.append((face,suit))
+    return deck
+
+
+
+if __name__ == "__main__":
+    deck = create_deck()
