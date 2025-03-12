1c4523b0b20fe0dc087d4d4f59fe637b5b989ca8
Kea Mmakola <keammakola@gmail.com>
2025-03-12 10:00:37 +0200

    created hand dealer function

--- game.py
+++ game.py
@@ -4,12 +4,14 @@ def create_deck():
     deck = []
     suits = ("hearts","diamonds","spades","clubs")
     faces = ("jack","queen","king","ace")
+    number_of_decks = 2
 
-    for suit in suits:
-        for i in range(2,11):
-            deck.append((i,suit))
-        for face in faces:
-            deck.append((face,suit))
+    for i in range(number_of_decks):
+        for suit in suits:
+            for i in range(2,11):
+                deck.append((i,suit))
+            for face in faces:
+                deck.append((face,suit))
     return deck
 def deck_shuffle(deck):
     random.shuffle(deck)
@@ -21,8 +23,20 @@ def player_creator(number:int):
     if number == 1:
         players.append("cpu")
     return players
+def deal_hands(players,deck):
+    player_hands = {}
+    for player in players:
+        hand = []
+        for i in range(8):
+            hand.append(deck[0])
+            deck.pop(0)
+        player_hands[player] = hand
+    return player_hands
 
 if __name__ == "__main__":
     deck = create_deck()
+    print(len(deck))
     shuffled_deck = deck_shuffle(deck)
     players = player_creator(int(input("How many players will be playing today?\nCrazy 8 allows 2-8 players.If you want to play with CPU, enter 1: ")))
+    hands = deal_hands(players,deck)
+    print(hands)
