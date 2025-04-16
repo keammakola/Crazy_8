6479c362e48c5e2277e26f10793db0065aef6838
kea mmakola <keammakola@gmail.com>
2025-04-16 02:17:33 +0200

    Added added a change colour feature

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ def create_deck():
             for rank in ranks:
                 deck.append((rank,suit))
 
-    return deck
+    return deck,suits
 def shuffler(deck):
     random.shuffle(deck)
 def player_creator(number:int):
@@ -44,9 +44,9 @@ def turn_selector(players):
     players.pop(0)
     return current_turn , players, next_turn
 def time_delay():
-    return time.sleep(2)
+    return time.sleep(0)
 def game_initialisation():
-    deck = create_deck()
+    deck,suits = create_deck()
     shuffler(deck)
     players_input = int(input("How many players will be playing today?\nCrazy 8 allows 2-8 players.If you want to play with CPU, enter 1: "))
     players = player_creator(players_input)
@@ -57,8 +57,7 @@ def game_initialisation():
         player_hands[player] = hand
     pile,deck = pile_creator(deck)
 
-
-    return deck , players , player_hands,pile
+    return deck , players , player_hands,pile,suits
 def take_two(next_hand,deck):
     return deal_cards(next_hand,2,deck)
 def skip(players):
@@ -66,7 +65,8 @@ def skip(players):
     players.pop(0)
     return players
 
-def game_loop(players,pile):
+def game_loop(players,pile,player_hands):
+    next_suit = "null"
     while True:
         terminal_clear()
         current_turn , players, next_turn= turn_selector(players)
@@ -74,18 +74,29 @@ def game_loop(players,pile):
         player_hand = player_hands[current_turn]
         print(f"It is {current_turn}'s turn.\n\nHere are your cards:\n{player_hand} \n\nCurrent card on the pile: {pile}\n")
         pile_card_number = pile[0]
-        pile_suit = pile[1]
+        if pile_card_number == 8:
+            pile_suit = next_suit
+        else:
+            pile_suit = pile[1]
         playable_cards = []
         for card in player_hand:
-            if card[0] == pile_card_number or card[1] == pile_suit:
-                if card not in playable_cards:
-                    playable_cards.append(card)
+            if next_suit == "null":
+                if card[0] == pile_card_number or card[1] == pile_suit or card[0] == 8:
+                    if card not in playable_cards:
+                        playable_cards.append(card)
+            else:
+                if card[1] == next_suit or card[0] == 8:
+                    if card not in playable_cards:
+                        playable_cards.append(card)
         deck_command = f"{len(playable_cards) + 1}. Pick card from deck."
         if current_turn == "cpu":
             time.sleep(2)
             played_card = random.choice(range(1, len(playable_cards) + 1))
         else:
-            print(f"Select one of the following playable options to play. Pick from 1 to {len(playable_cards) + 1}.")
+            if next_suit == "null":
+                print(f"Select one of the following playable options to play. Pick from 1 to {len(playable_cards) + 1}.")
+            else:
+                print(f"You can now play any card that is {next_suit} or an 8. Pick from 1 to {len(playable_cards) + 1}.")
 
             for i in playable_cards:
                 print(f"{playable_cards.index(i) + 1}. {i}")
@@ -96,6 +107,7 @@ def game_loop(players,pile):
         if played_card != deck_command:
             terminal_clear()
             print(f"{current_turn} has played {playable_cards[played_card - 1]}")
+            next_suit = "null"
             if playable_cards[played_card - 1][0] == 2:
                 take_two(next_hand,deck)
                 print(f"{current_turn} has made {next_turn} pick up 2 cards")
@@ -106,7 +118,21 @@ def game_loop(players,pile):
                 players.reverse()
                 players = skip(players)
                 print(f"{current_turn} has reversed the order of play.")
-            # add feature that accounts for change colour
+            elif playable_cards[played_card - 1][0] == 8:
+                suit_choice = int(input(
+                    f"You have played a 'change suits' card. Pick one colour you wish the next player should play\n1. {suits[0]}\n2. {suits[1]}\n3. {suits[2]}\n4. {suits[3]}\n "))
+                if suit_choice == 1:
+                    next_suit = suits[0]
+                elif suit_choice == 2:
+                    next_suit = suits[1]
+                elif suit_choice == 3:
+                    next_suit = suits[2]
+                elif suit_choice == 4:
+                    next_suit = suits[3]
+                else:
+                    print("Invalid choice, defaulting to Hearts.")
+                    next_suit = suits[0]
+
             time_delay()
             pile = playable_cards[played_card - 1]
             player_hand.remove(pile)
@@ -123,6 +149,6 @@ def game_loop(players,pile):
         continue
 
 if __name__ == "__main__":
-    deck,players,player_hands,pile = (game_initialisation())
-    game_loop(players,pile)
+    deck,players,player_hands,pile,suits = (game_initialisation())
+    game_loop(players,pile,player_hands)
 
